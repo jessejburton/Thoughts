92d2bb631d5087a403b827ec45f7bf321a715a00
Jesse James Burton <jesse@burtonmediainc.com>
2017-12-20 22:03:05 -0800

    Streaming thoughts site.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<h2>An experiment in thoughts.</h2>
 	<form name="add_thought_form" ng-submit="addThought()">
 		<div class="border"><input type="text" id="new_thought" name="new_thought" ng-model="newThought" required placeholder="What are you thinking?..."></div>
-		<button class="share-btn" ng-disabled="add_thought_form.$invalid">Share!</button>
+		<button class="share-btn" ng-disabled="add_thought_form.$invalid">Think!</button>
 	</form>
 
 	<div id="thoughts">
--- shared/header.php
+++ shared/header.php
@@ -7,7 +7,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300" rel="stylesheet" type="text/css" />
 	<link href="assets/css/style.css?v=1.7" rel="stylesheet" type="text/css" />
 
-	<meta name="viewport" content="width=500">
+	<meta name="viewport" content="width=300">
 
 </head>
 <body>
